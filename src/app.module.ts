421c7d7c53270a24f2ae42869ebfe047202a502c
ricardomenotti <ricardomenotti31@gmail.com>
2023-10-23 01:23:58 -0500

    authentication ready

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,20 @@ import { AppService } from './app.service';
 import { GradeModule } from './grade/grade.module';
 import { SchoolModule } from './school/school.module';
 import { AssigModule } from './assig/assig.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './users/auth.guard';
 
 @Module({
-  imports: [GradeModule, SchoolModule, AssigModule],
+  imports: [GradeModule, SchoolModule, AssigModule, AuthModule, UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class AppModule {}
---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,25 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { Public } from './constants';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Public()
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  signIn(@Body() signInDto: any) {
+    return this.authService.signIn(signInDto.cid, signInDto.password);
+  }
+}
